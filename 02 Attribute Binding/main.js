58eee5d16dbb6d996a882c45b12298952b08ed2b
Cosmin Andrei Artimof <caa8@st-andrews.ac.uk>
2024-05-11 19:31:59 +0100

    Lesson 2 commit

---
+++ 02 Attribute Binding/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://github.com/artimoai/vue3-fundamentals',
+    };
+  },
+});
